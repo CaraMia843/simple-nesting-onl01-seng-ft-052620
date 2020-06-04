d1b89c98b62ffeb13fc71ae35d3f74efbdc7b2ce
Cara Zellner <carajeanine@gmail.com>
2020-06-04 18:06:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,10 +84,10 @@ def adding_matz
      }
 programmer_hash[:yukihiro_matsumoto] = {
   known_for: "Ruby",
-  languages:["LISP", "C"]
-}
-end
+  languages:["LISP", "C"]}
+
 programmer_hash
+end
 
 def changing_alan
 	programmer_hash = 
